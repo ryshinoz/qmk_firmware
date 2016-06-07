10e8ce2c77d6e3c10a3014c5939a1f05302702d8
Ryosuke Shinozaki <ryshinoz@gmail.com>
2016-06-07 11:28:22 +0900

    Updated keymap

--- keyboard/ergodox_ez/keymaps/default/keymap.c
+++ keyboard/ergodox_ez/keymaps/default/keymap.c
@@ -50,7 +50,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
              TO(WINDOWS,1),      KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,             KC_BSLS,
                           KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,          KC_QUOT,
              TO(MOUSE,1), KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,          KC_RSFT,
-                                  KC_LBRC,  KC_RBRC, KC_EQL,LGUI(KC_LSFT),   MO(FN),
+                                  KC_LBRC,  KC_RBRC, KC_EQL,LGUI(KC_SPC),   MO(FN),
              KC_DOWN,       KC_UP,
              KC_PGUP,
              KC_PGDN,KC_SPC, KC_ENT
